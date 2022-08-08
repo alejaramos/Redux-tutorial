9c199b1b36176226908317ab90572536333ec121
alejaramos <iaramosr@gmail.com>
2022-08-08 17:23:28 -0400

    creating an action

---
+++ redux demo/index.js
@@ -0,0 +1,13 @@
+console.log("hola");
+//create an action 
+
+const buy_cake= "buy_cake"
+
+
+
+function buyCake(){
+    return {
+        type: buy_cake,
+        info:"first redux action"
+    }
+}
